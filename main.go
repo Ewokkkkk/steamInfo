01c525ff7371f2a68304af8ce839cc47a5b2ff20
Ewokkkkk <ewok.mfk@gmail.com>
2020-10-04 01:13:10 +0900

    add: some functions
    所持ゲーム数、総プレー時間、各ゲームのアイコン、名前、プレー時間の表示

--- main.go
+++ main.go
@@ -9,11 +9,46 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
-	"github.com/stretchr/objx"
+	"github.com/bitly/go-simplejson"
 )
+
+var apiKey string
+
+type Data struct {
+	Personaname   string
+	Profileurl    string
+	Avatarfull    string
+	GameCount     int
+	TotalPlaytime int
+	Games         []struct {
+		// Appid                    int
+		// Name                     string
+		// PlaytimeForever          int
+		// ImgIconURL               string
+		// ImgLogoURL               string
+		// PlaytimeWindowsForever   int
+		// PlaytimeMacForever       int
+		// PlaytimeLinuxForever     int
+		// HasCommunityVisibleStats bool
+		// Playtime2Weeks           int
+		Appid                    int    `json:"appid"`
+		Name                     string `json:"name"`
+		PlaytimeForever          int    `json:"playtime_forever"`
+		ImgIconURL               string `json:"img_icon_url"`
+		ImgLogoURL               string `json:"img_logo_url"`
+		PlaytimeWindowsForever   int    `json:"playtime_windows_forever"`
+		PlaytimeMacForever       int    `json:"playtime_mac_forever"`
+		PlaytimeLinuxForever     int    `json:"playtime_linux_forever"`
+		HasCommunityVisibleStats bool   `json:"has_community_visible_stats,omitempty"`
+		Playtime2Weeks           int    `json:"playtime_2weeks,omitempty"`
+		Price                    string
+	}
+}
 
 type PlayerSummaries struct {
 	Response struct {
@@ -40,6 +75,7 @@ type OwnedGames struct {
 			PlaytimeLinuxForever     int    `json:"playtime_linux_forever"`
 			HasCommunityVisibleStats bool   `json:"has_community_visible_stats,omitempty"`
 			Playtime2Weeks           int    `json:"playtime_2weeks,omitempty"`
+			Price                    string
 		} `json:"games"`
 	} `json:"response"`
 }
@@ -99,7 +135,6 @@ func getUserGameList(apiKey, steamid string) OwnedGames {
 	params.Add("include_appinfo", "true")
 	params.Add("format", "json")
 	request.URL.RawQuery = params.Encode()
-
 	fmt.Println(request.URL.String())
 
 	timeout := time.Duration(5 * time.Second)
@@ -126,6 +161,63 @@ func getUserGameList(apiKey, steamid string) OwnedGames {
 	return games
 }
 
+func getPriceJPY(Appids []string) string {
+	url := "https://store.steampowered.com/api/appdetails"
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	id := strings.Join(Appids, ",")
+	// id = id + id + id + id
+	params := request.URL.Query()
+	params.Add("appids", id)
+	params.Add("filters", "price_overview")
+	request.URL.RawQuery = params.Encode()
+	fmt.Println(request.URL.String())
+	timeout := time.Duration(5 * time.Second)
+	client := &http.Client{
+		Timeout: timeout,
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	js, err := simplejson.NewJson(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, err := js.Map()
+	if err != nil {
+		log.Fatal(err)
+	}
+	keys := make([]string, 0, len(m))
+	for k, _ := range m {
+		keys = append(keys, k)
+	}
+
+	price, err := js.GetPath(keys[0], "data", "price_overview", "initial_formatted").String()
+	if err != nil {
+		return "--"
+	}
+	// セール中でなければinitial_formattedは空なのでfinal_formattedから取得する
+	if price == "" {
+		price, err = js.GetPath(keys[0], "data", "price_overview", "final_formatted").String()
+		if err != nil {
+			return "--"
+		}
+	}
+	return price
+
+}
+
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ =
@@ -134,32 +226,66 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	data := map[string]interface{}{
 		"Host": r.Host,
 	}
-	if authCookie, err := r.Cookie("auth"); err == nil {
-		data["UserData"] = objx.MustFromBase64(authCookie.Value)
-	}
 
 	t.templ.Execute(w, data)
 }
 
-func main() {
-	apiKey := os.Getenv("STEAM_APIKEY")
-	steamid := "76561198051101724" // my id
-	playerSummaries := GetPlayerSummaries(apiKey, steamid)
-	ownedGames := getUserGameList(apiKey, steamid)
-	playTime := 0
+// テンプレート用独自関数
+func HourTimes(i int) int {
+	return i / 60
+}
+
+func user(w http.ResponseWriter, r *http.Request) {
+	var appids []string
+
+	fmt.Println("method:", r.Method)
+	id := r.FormValue("userid")
+	playerSummaries := GetPlayerSummaries(apiKey, id)
+	ownedGames := getUserGameList(apiKey, id)
+	TotalPlaytime := 0
 	gameCount := ownedGames.Response.GameCount
 
 	fmt.Println(playerSummaries.Response.Players[0].Personaname)
 	fmt.Println("game count:", gameCount)
 	for _, val := range ownedGames.Response.Games {
 		fmt.Printf("%-40v% 5vh\n", val.Name, val.PlaytimeForever/60)
+		TotalPlaytime += val.PlaytimeForever
+		// val.Price = getPriceJPY(val.Appid)
+		// time.Sleep(time.Second * 5)
+		appids = append(appids, strconv.Itoa(val.Appid))
 	}
-	for _, val := range ownedGames.Response.Games {
-		playTime += val.PlaytimeForever
+	// getPriceJPY(appids) // test
+
+	fmt.Printf("total:% 40vh\n", TotalPlaytime/60)
+
+	funcs := template.FuncMap{
+		"HourTimes": HourTimes,
 	}
-	fmt.Printf("total:% 40vh\n", playTime/60)
+
+	t, err := template.New("user.html").Funcs(funcs).ParseFiles("templates/user.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := Data{
+		Personaname:   playerSummaries.Response.Players[0].Personaname,
+		Profileurl:    playerSummaries.Response.Players[0].Profileurl,
+		Avatarfull:    playerSummaries.Response.Players[0].Avatarfull,
+		GameCount:     ownedGames.Response.GameCount,
+		TotalPlaytime: TotalPlaytime / 60,
+		Games:         ownedGames.Response.Games,
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	apiKey = os.Getenv("STEAM_APIKEY")
+	//76561198051101724
 
 	http.Handle("/", &templateHandler{filename: "index.html"})
+	http.HandleFunc("/user", user)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
