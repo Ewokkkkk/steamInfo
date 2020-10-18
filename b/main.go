bff3be375ee6aa9dc2efdd98a5cdd8bac8724567
Ewokkkkk <ewok.mfk@gmail.com>
2020-10-18 23:25:52 +0900

    add: 全ゲームの発売日、価格を取得
    DBに格納する前の準備

--- b/main.go
+++ b/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
+	"github.com/bitly/go-simplejson"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -17,56 +18,54 @@ type GameList struct {
 		Apps []struct {
 			Appid int    `json:"appid"`
 			Name  string `json:"name"`
+			Price int
+			Date  string
 		} `json:"apps"`
 	} `json:"applist"`
 }
 
 type GameInfo struct {
-	Success bool `json:"success"`
-	Data    struct {
-		PriceOverview struct {
-			// Currency         string `json:"currency"`
-			Initial int `json:"initial"`
-			// Final            int    `json:"final"`
-			// DiscountPercent  int    `json:"discount_percent"`
-			// InitialFormatted string `json:"initial_formatted"`
-			// FinalFormatted   string `json:"final_formatted"`
-		} `json:"price_overview"`
-		ReleaseDate struct {
-			// ComingSoon bool   `json:"coming_soon"`
-			Date string `json:"date"`
-		} `json:"release_date"`
-	} `json:"data"`
+	Price int
+	Date  string
 }
+
+var GamesInfo []GameInfo
 
 func main() {
 	games := getGameList()
+	// for _, val := range games.Applist.Apps {
+	// 	getGamesInfo(val.Appid)
+	// 	fmt.Println(GamesInfo)
+	// 	time.Sleep(time.Second * 3)
+	// }
+	getGamesInfo(games)
+	fmt.Println(games.Applist.Apps[0].Date)
 
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:8889)/steam-info-db")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
+	// db, err := sql.Open("mysql", "root:root@tcp(localhost:8889)/steam-info-db")
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// defer db.Close()
 
-	stmtInsert, err := db.Prepare("INSERT INTO games_info(id, name) VALUES(?, ?)")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer stmtInsert.Close()
-	for _, val := range games.Applist.Apps {
-		id := val.Appid
-		name := val.Name
-		result, err := stmtInsert.Exec(id, name)
-		if err != nil {
-			panic(err.Error())
-		}
-		lastInsertID, err := result.LastInsertId()
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Println(lastInsertID)
+	// stmtInsert, err := db.Prepare("INSERT INTO games_info(id, name) VALUES(?, ?)")
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// defer stmtInsert.Close()
+	// for _, val := range games.Applist.Apps {
+	// 	id := val.Appid
+	// 	name := val.Name
+	// 	result, err := stmtInsert.Exec(id, name)
+	// 	if err != nil {
+	// 		panic(err.Error())
+	// 	}
+	// 	lastInsertID, err := result.LastInsertId()
+	// 	if err != nil {
+	// 		panic(err.Error())
+	// 	}
+	// 	fmt.Println(lastInsertID)
 
-	}
+	// }
 }
 
 func getGameList() GameList {
@@ -98,4 +97,68 @@ func getGameList() GameList {
 	}
 
 	return gameList
+}
+
+// func getGamesInfo(Appid int) {
+func getGamesInfo(games GameList) {
+	cnt := 0
+	for _, val := range games.Applist.Apps {
+
+		url := "https://store.steampowered.com/api/appdetails"
+		request, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		params := request.URL.Query()
+		params.Add("filters", "price_overview,release_date")
+		params.Add("cc", "jpy")
+		params.Add("l", "japanese")
+		params.Add("appids", strconv.Itoa(val.Appid))
+		request.URL.RawQuery = params.Encode()
+		fmt.Println(request.URL.String())
+
+		timeout := time.Duration(5 * time.Second)
+		client := &http.Client{
+			Timeout: timeout,
+		}
+
+		response, err := client.Do(request)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer response.Body.Close()
+
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		js, err := simplejson.NewJson(body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		m, err := js.Map()
+		if err != nil {
+			log.Fatal(err)
+		}
+		keys := make([]string, 0, len(m))
+		for k, _ := range m {
+			keys = append(keys, k)
+		}
+		price, err := js.GetPath(keys[0], "data", "price_overview", "initial").Int()
+		if err != nil {
+			price = -1
+		}
+
+		date, err := js.GetPath(keys[0], "data", "release_date", "date").String()
+		if err != nil {
+			date = "-"
+		}
+		val.Price = price
+		val.Date = date
+
+		time.Sleep(time.Second * 3)
+
+	}
 }
